6387cc1b32260916d610a3fd315b5a9a1bb8cdca
José Luis Ramírez Morales <sach13lrm@gmail.com>
2020-11-01 12:00:40 -0600

    Reading query and body

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function (req, res) {
+  console.log(req.query)
   res.send('Lista de mensajes')
-})
+});
 
 router.post('/message', function (req, res) {
-  res.send('Mensaje añadido')
-})
+  console.log(req.query)
+  console.log(req.body)
+  res.send('Mensaje '+ req.body.text + ' añadido correctamente')
+});
+
 // app.use('/', function(req, res){
   //   res.send('Hola')
 // });
 
 app.listen(3000);
 
-console.log('Escuchando en http://localhost:3000')
+console.log('Escuchando en http://localhost:3000');
