dda29b41e8885ca3055bfa18ccff93e435c39e60
José Luis Ramírez Morales <sach13lrm@gmail.com>
2020-11-01 12:44:17 -0600

    Error logs

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ router.get('/message', function (req, res) {
   res.header({
     "custom-header": "Nuestro valor personalizado"
   })
-
   response.success(req, res, 'Lista de mensajes');
 });
 
@@ -23,7 +22,7 @@ router.post('/message', function (req, res) {
   console.log(req.query);
   console.log(req.body);
   if(req.query.error ==  'ok'){
-    response.error(req, res, 'Error simulado', 400)
+    response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación de los errores')
   }else{
     response.success(req, res, 'Creado correctamente', 201);
   }
